0c76b0a98f721ab203d7dc69d3dcffa635ad40f5
Bhavesh Daswani <32614748+bhaveshdaswani93@users.noreply.github.com>
2021-03-13 09:54:26 +0530

    Between Two Sets hackerrank solution in php

---
+++ Between_Two_Sets.php
@@ -0,0 +1,38 @@
+function getTotalX($a, $b) {
+    // Write your code here
+    usort($a,function($x,$y){
+       return $x <=> $y; 
+    });
+    usort($b,function($x,$y){
+       return $x <=> $y; 
+    });
+    
+    $min = $a[count($a)-1];
+    $max = $b[0];
+    
+    $ans = 0;
+    for($i=$min;$i<=$max;$i++)
+    {
+        $toConsider = true;
+        for($j=0;$j<count($a);$j++){
+            if($i%$a[$j] !== 0  ){
+                $toConsider = false;
+                break;
+            }
+        }
+        
+        for($k=0;$k<count($b);$k++){
+            if($b[$k]%$i !== 0) {
+                $toConsider = false;
+                break;
+            }
+        }
+        
+        if($toConsider){
+           $ans++; 
+        }
+        
+    }
+    return $ans;
+
+}
